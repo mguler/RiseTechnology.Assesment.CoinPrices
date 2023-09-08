5d7103b4c08ae2fec83e3b651ccccd26617a21ca
murat <murat.guler@lycos.com>
2023-09-09 00:36:11 +0300

    Jwt Token servisi eklendi

---
+++ RiseTechnology.Assesment.CoinPrices.Core/Abstract/Auth/IJwtService.cs
@@ -0,0 +1,7 @@
+﻿namespace RiseTechnology.Assesment.CoinPrices.Core.Abstract.Auth
+{
+    public interface IJwtService
+    {
+        string GenerateToke(string key, string issuer, string audience, int expirationInMinutes, Dictionary<string, string> claims);
+    }
+}
