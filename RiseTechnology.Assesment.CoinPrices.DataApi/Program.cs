be73e0feec9717b31564e8d15f1527f281a53942
murat <murat.guler@lycos.com>
2023-09-18 02:41:42 +0300

    Program.cs Configurasyon duzenlendi

--- RiseTechnology.Assesment.CoinPrices.DataApi/Program.cs
+++ RiseTechnology.Assesment.CoinPrices.DataApi/Program.cs
@@ -98,7 +98,4 @@ app.UseAuthorization();
 app.MapControllerRoute(name: "get-prices", pattern: "get-prices-{priceInfoFilter:regex(today|month|year)}", defaults: new { controller = "CoinManagement", action = "GetPrices" });
 #endregion End Of Coin Management
 
-var dbContext = app.Services.CreateScope().ServiceProvider.GetService<DbContext>();
-dbContext.Database.EnsureCreated();
-
 app.Run();
